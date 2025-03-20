9e251950e173f163f55f6796db227e9a3fac995a
azishanansari4587 <azishanansari4587@gmail.com>
2025-03-20 14:18:28 +0530

    If Else Statement in Java

---
+++ src/JavaBasics/Day3/IfElseStatement.java
@@ -0,0 +1,17 @@
+package JavaBasics.Day3;
+
+public class IfElseStatement {
+
+    public static void main(String[] args) {
+
+        //* TODO if-else statement: a block of code that is executed if a certain condition is true, and another block of code that is executed if the condition is false.
+
+        int num = 10; //* 10 is an even number then condition will be true
+
+        if ( num % 2 == 0 ) {
+            System.out.println("The number is even.");
+        } else {
+            System.out.println("The number is odd.");
+        }
+    }
+}
