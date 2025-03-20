c3a3c77dd654c2e61c35a835d3ed5c51348253dc
azishanansari4587 <azishanansari4587@gmail.com>
2025-03-20 14:18:07 +0530

    If Statement in Java

---
+++ src/JavaBasics/Day3/IfStatement.java
@@ -0,0 +1,15 @@
+package JavaBasics.Day3;
+
+public class IfStatement {
+
+    public static void main(String[] args) {
+
+        //* TODO if statement: a block of code that is executed if a certain condition is true.
+
+        int age = 20;
+
+        if (age >= 18) {
+            System.out.println("You are eligible to vote.");
+        }
+    }
+}
