9cd8eef9f6c4fd6161c1aa0fc56aeebbff9e08f8
azishanansari4587 <azishanansari4587@gmail.com>
2025-03-20 14:05:36 +0530

    Introduction of Java

---
+++ src/JavaBasics/Day1/IntroJava.java
@@ -0,0 +1,26 @@
+package JavaBasics.Day1;
+
+public class IntroJava { //* Defines a class named => "IntroJava"
+
+    public static void main(String[] args) { //* Entry point of Java program
+
+        //! Ques: What is Java?
+        //* Ans: Java is a high-level, object-oriented programming language. It is developed by James Gosling at Sun Microsystems in 1995.
+
+        //? 🚀 Features of Java
+        //*    ✅ Platform-independent (Runs on Windows, Mac, Linux, etc.)
+        //*    ✅ Object-Oriented (Follows OOP principles)
+        //*    ✅ Secure (Runs inside JVM, avoids direct memory access)
+        //*    ✅ Multi-threaded (Supports concurrent execution)
+        //*    ✅ Robust & Memory Managed (Automatic Garbage Collection)
+
+        //! Ques: What is the difference between JVM, JRE, and JDK?
+        //* Ans: JVM: Java Virtual Machine, It Converts bytecode to machine code and executes the code.
+        //*      JRE: Java Runtime Environment, It provides the Java platform and includes the JVM.
+        //*      JDK: Java Development Kit, It includes the JRE and development tools.
+
+        //* TODO: JDK = JRE + JVM + Compiler + Debugger
+
+        System.out.println("Welcome to Java Programming!"); //* Prints text to the console.
+    }
+}
