6b0addb7bca757bada9853b5385e7c695ad1355c
azishanansari4587 <azishanansari4587@gmail.com>
2025-03-20 14:20:30 +0530

    While (As long as) Loops iterate in Java

---
+++ src/JavaBasics/Day3/WhileLoop.java
@@ -0,0 +1,15 @@
+package JavaBasics.Day3;
+
+public class WhileLoop {
+
+    public static void main(String[] args) {
+
+        //* TODO while loop: a block of code that is executed as long as a certain condition is true.
+        int i = 1;
+
+        while (i <= 5) {
+            System.out.println("Iteration: "+i);
+            i++;
+        }
+    }
+}
