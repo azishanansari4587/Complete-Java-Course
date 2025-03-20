d046c7995dd67ad113f8b82b826aced677208ed3
azishanansari4587 <azishanansari4587@gmail.com>
2025-03-20 14:15:52 +0530

    DeclareVariables for Name, Age and Salary.

---
+++ src/JavaBasics/Day2/Practice/DeclareVariables.java
@@ -0,0 +1,17 @@
+package JavaBasics.Day2.Practice;
+
+public class DeclareVariables {
+
+    public static void main(String[] args) {
+
+        //* ✅ Write a program that declares variables for your name, age, and salary, and prints them.
+
+        String name = "Zishan Ansari";
+        int age = 25;
+        double salary = 20000.0;
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+    }
+}
